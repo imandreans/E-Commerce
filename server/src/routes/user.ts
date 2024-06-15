8f30c45b381f262ff219b04118c6cefb81ef38d0
vAndrean-drag <andrean.sihombing83@gmail.com>
2024-06-15 18:50:06 +0800

    changes in user

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,4 +1,4 @@
-import { bcrypt } from "bcryptjs";
+import bcrypt from "bcryptjs";
 import { Router, Request, Response, NextFunction } from "express";
 import { IUser, UserModel } from "../models/user";
 import { UserErrors } from "../errors";
