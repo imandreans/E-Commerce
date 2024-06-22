23c64fdc1457ed7770be185808c7e7845eb605de
vAndrean-drag <andrean.sihombing83@gmail.com>
2024-06-22 14:42:26 +0800

    fix error when refresh and logout

--- client/react-app/src/context/shop-context.tsx
+++ client/react-app/src/context/shop-context.tsx
@@ -151,7 +151,7 @@ export const ShopContextProvider = (props) => {
     } else {
       setIsAuthenticated(false);
     }
-  });
+  }, [isAuthenticated]);
   useEffect(() => {
     if (!isAuthenticated) {
       localStorage.clear();
@@ -162,8 +162,6 @@ export const ShopContextProvider = (props) => {
   // synchronize a component, triggered when the shopcontext provider is called
   useEffect(() => {
     if (isAuthenticated) {
-      fetchAvailableMoney();
-      fetchPurchasedItems();
     }
     // send value of isAuthenticated
   }, [isAuthenticated]);
